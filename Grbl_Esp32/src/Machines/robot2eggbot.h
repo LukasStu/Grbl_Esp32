5e16cd4a7774165ad51318d9481b43956a27f385
Lukas Stürmlinger <lukas.stuermlinger@sew-eurodrive.de>
2026-03-12 19:24:04 +0100

    fix gpios

--- Grbl_Esp32/src/Machines/robot2eggbot.h
+++ Grbl_Esp32/src/Machines/robot2eggbot.h
@@ -16,18 +16,18 @@
 
 // Motor GPIO mapping from the conversion plan.
 #define X_STEP_PIN      GPIO_NUM_14
-#define Y_STEP_PIN      GPIO_NUM_12
+#define Y_STEP_PIN      GPIO_NUM_26
 
-#define X_DIRECTION_PIN GPIO_NUM_25
-#define Y_DIRECTION_PIN GPIO_NUM_26
+#define X_DIRECTION_PIN GPIO_NUM_27
+#define Y_DIRECTION_PIN GPIO_NUM_25
 
-#define STEPPERS_DISABLE_PIN GPIO_NUM_13
+#define STEPPERS_DISABLE_PIN GPIO_NUM_12
 
 #define X_LIMIT_PIN     GPIO_NUM_15
 #define Y_LIMIT_PIN     GPIO_NUM_4
 
 // Standard eggbot pen output, remapped to the Robot2Eggbot servo pin.
-#define SPINDLE_OUTPUT_PIN          GPIO_NUM_27
+#define SPINDLE_OUTPUT_PIN          GPIO_NUM_13
 #define SPINDLE_TYPE                SpindleType::PWM
 #define DEFAULT_SPINDLE_RPM_MIN     0.0  // $31 rpm
 #define DEFAULT_LASER_MODE          1    // $32 false
@@ -39,10 +39,10 @@
 //#define DEFAULT_SPINDLE_MAX_VALUE   12.0   // $36 7864, Percent of full period 65536 (extended set)
 
 // SPI pin remap for the Micro SD board from the conversion plan.
-#define GRBL_SPI_SCK    GPIO_NUM_22
+#define GRBL_SPI_SCK    GPIO_NUM_18
 #define GRBL_SPI_MISO   GPIO_NUM_19
-#define GRBL_SPI_MOSI   GPIO_NUM_21
-#define GRBL_SPI_SS     GPIO_NUM_33
+#define GRBL_SPI_MOSI   GPIO_NUM_23
+#define GRBL_SPI_SS     GPIO_NUM_5
 
 #define DEFAULT_X_HOMING_MPOS   0.0
 #define DEFAULT_Y_HOMING_MPOS   0.0
